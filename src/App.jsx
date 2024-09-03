971021dc681d5e619f0dd2438956a966ccf1d6df
MandyX <deepanddipyo@gmail.com>
2024-09-03 13:25:43 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <h1 className='font-titles font-[700] text-7xl' >WE CREATE EYE-OPENING PRESENTATIONS</h1>
+      <p>This is a para</p>
+    </main>
+  )
+}
+
+export default App
