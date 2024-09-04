34cf03da2a8d6b7769c34f9a31ca80ec72244618
MandyX <deepanddipyo@gmail.com>
2024-09-04 22:42:35 +0530

    json file added

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CallToAction = () => {
+
+  const [info, setInfo] = useState(null);
+
+  useEffect(() => {
+    fetch("src/data/callToAction.json")
+      .then(response => response.json())
+      .then(data => setInfo(data));
+  }, []);
+  if (!info) {
+    return null;
+  }
+
   return (
     <section className='pt-[10.6vh]'>
-        <h1 className='font-titles font-[700] text-7xl' >WE CREATE EYE-OPENING PRESENTATIONS</h1>
-        <p>This is a para</p>
+        {info.titles.map((title, i) => (
+          <span key={i} className='bg-black'>
+            <div className=''>
+              <h1 className='font-titles font-[700] text-7xl uppercase' >{title}</h1>
+            </div>
+          </span>
+        ))}
+        <p>{info.message}</p>
     </section>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
     <nav className='fixed backdrop-blur-sm bg-[rgba(241,241,241,0.3)] w-full grid grid-cols-[48%_42%_10%] justify-between py-[1.6vh] px-[5vw]'>
         <a href=""><img src="./src/assets/icon3.png" alt="Work Projects" className='w-60' /></a>
         <ul className='flex gap-[0.6vw] items-center'>
-            <li className='w-24 text-center border-b-2 border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,1)] hover:tracking-widest active:font-semibold transition-all'>Home</li>
-            <li className='w-24 text-center border-b-2 border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,1)] hover:tracking-widest active:font-semibold transition-all'>Projects</li>
-            <li className='w-24 text-center border-b-2 border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,1)] hover:tracking-widest active:font-semibold transition-all'>About Me</li>
-            <li className='w-24 text-center border-b-2 border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,1)] hover:tracking-widest active:font-semibold transition-all'>Résumé</li>
+            <li className='w-24 text-center border-b-[1.6px] border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,0.8)] hover:tracking-widest active:font-semibold transition-all'>Home</li>
+            <li className='w-24 text-center border-b-[1.6px] border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,0.8)] hover:tracking-widest active:font-semibold transition-all'>Projects</li>
+            <li className='w-24 text-center border-b-[1.6px] border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,0.8)] hover:tracking-widest active:font-semibold transition-all'>About Me</li>
+            <li className='w-24 text-center border-b-[1.6px] border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,0.8)] hover:tracking-widest active:font-semibold transition-all'>Résumé</li>
         </ul>
-        <li className='w-24 self-center list-none text-center border-b-2 border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,1)] hover:tracking-widest active:font-semibold transition-all'>Contacts</li>
+        <li className='w-24 self-center list-none text-center border-b-[1.6px] border-[rgba(43,43,43,0)] hover:border-[rgba(43,43,43,0.8)] hover:tracking-widest active:font-semibold transition-all'>Contacts</li>
     </nav>
   )
 }
