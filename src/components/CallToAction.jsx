a2ed87761f2d91dd46b0f442ebf1bec5082f8365
MandyX <deepanddipyo@gmail.com>
2024-09-14 15:14:41 +0530

    issues fixed, new features added

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -41,7 +41,7 @@ const CallToAction = () => {
 
   return (
     <section id='home' className={`
-      pt-[24vh] select-none
+      md:pt-[24vh] pt-[15vh] select-none
       transition-all duration-500 ease-in-out
       ${visibleMsg.includes(2) ? 'h-[93vh]' : 'h-screen'}
     `}>
@@ -52,31 +52,47 @@ const CallToAction = () => {
             ${visibleIndices.includes(i) ? 'w-full' : 'w-2'}
           `}>
           <div className={`
-                  lg:text-[6.5rem] md:text-[5.5rem] sm:text-[4.5rem] text-[2.6rem] 
-                  font-titles sm:font-[700] font-[700] uppercase leading-[0.85]
+                  lg:text-[6.5rem] md:text-[5.5rem] sm:text-[4.5rem] text-[3rem] 
+                  font-titles sm:font-[700] font-[700] uppercase sm:leading-[0.85] leading-none 
+                  sm:tracking-tight md:tracking-normal tracking-tighter
                   transition-all duration-[1500ms] ease-in-out transform
                   ${visibleIndices.includes(i) ? 'opacity-100' : 'opacity-0'}
                 `} >{title}
           </div>
         </div>
       ))}
-      <div className='md:flex block py-[5vh] px-[5vw] mt-[14vh] justify-between border-t-[1.5px] border-[#b2b2b2]'>
+      <a href="https://github.com/Manideep-X?tab=repositories" target='_blank' className={`
+          sm:hidden transition-all duration-500 ease-in-out transform w-[55%]
+          ${visibleMsg.includes(2) ? 'opacity-100' : 'opacity-0'}
+        `}>
+        <div className={`
+          text-[0.86rem]
+          flex items-center justify-between bg-red-500 
+          shadow-[-5px_10px_18px_-4px_rgba(0,0,0,0.45)]
+          ml-[5.5vw] mt-4 w-[52%] px-4 py-[0.6rem]
+          transition-all duration-200 ease-in-out
+        `}>
+          <p className='text-white'>See all projects in</p>
+          <img src="src/assets/github-mark-white.svg" alt="GitHub" className='w-7 pl-[0.8px]' />
+        </div>
+      </a>
+      <div className='md:flex block py-[5vh] px-[5vw] md:mt-[14vh] mt-[16vh] justify-between border-t-[1.5px] border-[#b2b2b2]'>
         <div className='content-center'>
           {info.messages.map((msg, j) => (
             <p key={j} className={`
-                md:inline block text-[1rem]
+                md:inline block sm:text-[1rem] text-[0.9rem] md:py-0 py-2
                 transition-all duration-500 ease-in-out transform
                 ${visibleMsg.includes(j) ? 'opacity-100' : 'opacity-0'}
               `}>{msg}
             </p>
           ))}
         </div>
-        <div className={`
+        <div className={`py-8 md:py-0
             transition-all duration-300 ease-in-out transform
             ${visibleMsg.includes(2) ? 'opacity-100' : 'opacity-0'}
           `}>
-          <a href='#about-me' className={`text-center block
-            uppercase px-3 py-1 border-2 rounded-full border-[rgba(43,43,43,0.8)]
+          <a href='#about-me' className={`text-center md:block sm:text-base text-[0.8rem]
+            uppercase px-3 py-1 md:border-2 border-[1px] rounded-full border-[rgba(43,43,43,0.8)]
             hover:bg-[rgb(43,43,43)] hover:text-[#f1f1f1] cursor-pointer
             transition-all duration-200
           `}>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 const Resume = () => {
   return (
     <article id='resume' className='
-      bg-[rgba(0,77,67,0.9)] text-[#f1f1f1] rounded-t-3xl px-[4vw] py-[10vh]
+      bg-[rgba(0,77,67,0.9)] text-[#f1f1f1] md:rounded-t-3xl rounded-t-2xl px-[4vw] py-[10vh]
       h-[16vh] hover:h-[35vh] transition-all duration-300 ease-in-out
     '>
       This is "resume" section
